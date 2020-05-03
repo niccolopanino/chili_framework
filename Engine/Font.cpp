18f99cea8724e7d2bf30c23fde68327a85e35477
Nikolai Metscher <nikolai@metscher.de>
2020-05-03 22:05:57 +0200

    added font class for text rendering

---
+++ Engine/Font.cpp
@@ -0,0 +1,41 @@
+#include "Font.h"
+#include <cassert>
+
+Font::Font(const std::string &filename, Color chroma) :
+    m_surf(filename),
+    m_glyph_width(m_surf.get_width() / k_num_columns),
+    m_glyph_height(m_surf.get_height() / k_num_rows),
+    m_chroma(chroma)
+{
+    // verify that bitmap had valid dimensions
+    assert(m_glyph_width * k_num_columns == m_surf.get_width());
+    assert(m_glyph_height * k_num_rows == m_surf.get_height());
+}
+
+void Font::draw_text(const std::string &text, const Vec2i &pos, Color color, Graphics &gfx) const
+{
+    // cur_pos is the pos that we are drawing to on the screen
+    auto cur_pos = pos;
+    for (auto c : text) {
+        // only draw characters that are on the font sheet
+        // k_first_char + 1 because might as well skip ' ' as well
+        if (c >= k_first_char + 1 && c <= k_last_char)
+            gfx.draw_sprite_substitute(cur_pos.m_x, cur_pos.m_y, color,
+                map_glyph_rect(c), m_surf, m_chroma);
+        // advance screen pos for next character
+        cur_pos.m_x += m_glyph_width;
+    }
+}
+
+IRect Font::map_glyph_rect(char c) const
+{
+    assert(c >= k_first_char && c <= k_last_char);
+    // font sheet glyphs start at ' ', calculate index into sheet
+    const int glyph_index = c - ' ';
+    // map 1d glyph index to 2d coordinates
+    const int y_glyph = glyph_index / k_num_columns;
+    const int x_glyph = glyph_index % k_num_columns;
+    // convert the sheet grid coords to pixel coords in sheet
+    return IRect({ x_glyph * m_glyph_width, y_glyph * m_glyph_height },
+        m_glyph_width, m_glyph_height);
+}
---
+++ Engine/Font.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Graphics.h"
+#include "Surface.h"
+#include "Vec2i.h"
+
+class Font
+{
+public:
+    Font(const std::string &filename, Color chroma = Colors::White);
+    void draw_text(const std::string &text, const Vec2i &pos, Color color, Graphics &gfx) const;
+private:
+    IRect map_glyph_rect(char c) const;
+private:
+    // holds the font sheet bitmap data
+    Surface m_surf;
+    // this gives the dimensions of a glyph in the font sheet
+    int m_glyph_width;
+    int m_glyph_height;
+    // font sheet chroma color
+    Color m_chroma;
+private:
+    // number of rows / columns in the font sheet (this is fixed)
+    static constexpr int k_num_columns = 32;
+    static constexpr int k_num_rows = 3;
+    // start and end drawable character codes
+    static constexpr char k_first_char = ' ';
+    static constexpr char k_last_char = '~';
+};
