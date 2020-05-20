9bbe17eba718deddd4701b371534ffeec96b2cc0
Nikolai Metscher <nikolai@metscher.de>
2020-05-20 23:25:36 +0200

    suzanne lighted per pixel

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -65,6 +65,10 @@ Game::Game(MainWindow &wnd) : m_wnd(wnd), m_gfx(wnd)
         m_gfx,
         IndexedTriangleList<GouraudPointScene::Vertex>::load_normals("resources/models/suzanne.obj")
     ));
+    m_scenes.push_back(std::make_unique<PhongPointScene>(
+        m_gfx,
+        IndexedTriangleList<PhongPointScene::Vertex>::load_normals("resources/models/suzanne.obj")
+    ));
     m_cur_scene = m_scenes.begin();
     output_scene_name();
 }
